c201b2e69de26b6515a5ee0b63c7778b74b4dcfd
Odzen <jsebastian.va@gmail.com>
2023-04-04 01:14:58 -0500

    operations

--- Vector3.h
+++ Vector3.h
@@ -1,6 +1,11 @@
 #ifndef VECTOR3_H
 #define VECTOR3_H
 
+#include <iostream>
+# include <math.h>
+
+using namespace std;
+
 class Vector3
 {
     public:
@@ -21,6 +26,69 @@ class Vector3
         {
             return Vector3(x + v.x, y + v.y, z + v.z);
         }
+
+        Vector3 operator-=(const Vector3 &v)
+        {
+            return (*this = (*this - v));
+        }
+
+        Vector3 operator-(const Vector3 &v)
+        {
+            return Vector3(x - v.x, y - v.y, z - v.z);
+        }
+
+        Vector3 operator*=(float number)
+        {
+            return (*this = (*this * number));
+        }
+
+        Vector3 operator*(float number)
+        {
+            return Vector3(x * number, y * number, z * number);
+        }
+
+        Vector3 operator/=(float number)
+        {
+            return (*this = (*this / number));
+        }
+
+        Vector3 operator/(float number)
+        {
+            return Vector3( x / number, y / number, z / number);
+        }
+
+        Vector3 operator-(void)
+        {
+            return Vector3(-x, -y, -z);
+        }
+
+        // cross product function
+        Vector3 operator*(const Vector3 &v){
+            return Vector3(y*v.z - z*v.y, z*v.x - x*v.z, x*v.y - y*v.x);
+        };
+
+        // dot product function
+        float Dot(Vector3 v){
+            return (x*v.x + y*v.y + z*v.z);
+        };
+
+        float Length(void){
+            return sqrt(x*x + y*y + z*z);
+        };
+
+        Vector3 Normalize(void){
+            float length = Length();
+
+            x/=length;
+            y/=length;
+            z/=length;
+
+            return *this;
+        };
+
+        void print(void){
+            cout << "Vector: " << x << ", " << y << ", " << z << endl;
+        };
 
     public:
         float x;
--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
-#include<stdio.h>
-/* demo.c:  My first C program on a Linux */
+
+#include <iostream>
+#include "Vector3.h"
+
+using namespace std;
 int main(void)
 {
- printf("Hello! This is a test prgoram.\n");
- return 0;
+    Vector3 vectorPlus = Vector3(1, 2, 3) + Vector3(7, 2, 5);
+    Vector3 vectorSubstract = Vector3(1, 2, 3) - Vector3(7, 2, 5);
+    Vector3 vectorMulti = Vector3(4, 5, 9) * 5;
+    float dotValue = Vector3(1, 2, 3).Dot(Vector3(3, 2, 1));
+    Vector3 vectorCross = Vector3(9, 4, 6) * Vector3(6, 1, 7);
+    Vector3 vectorNorm = Vector3(1, 2, 3).Normalize();
+    
+    vectorPlus.print();
+    vectorSubstract.print();
+    vectorMulti.print();
+    cout << dotValue << endl;
+    vectorCross.print();
+    vectorNorm.print();
 }
