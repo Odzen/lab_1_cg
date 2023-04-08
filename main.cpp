bc1752c27be6167d527d88ee6fc671ff29a206ce
SebastiánTV <107218018+xlPlanetarizaGol@users.noreply.github.com>
2023-04-08 09:45:16 -0500

    Update main.cpp
    code structuring and updating

--- main.cpp
+++ main.cpp
@@ -7,13 +7,14 @@ using namespace std;
 float x = 0.0f; // Posición horizontal del proyectil en metros
 float y = 0.0f; // Posición vertical del proyectil en metros
 
-void drawPerson() {
-    // Cabeza
+void drawFigures() {
+    
+    // Esfera
     glColor3f(1.0f, 0.0f, 0.0f);
     glTranslatef(0.0f, 2.0f, 0.0f);
     glutSolidSphere(1.0f, 20, 20);
 
-    // Torso
+    // Triangulo Izquierdo
     glColor3f(0.0f, 1.0f, 0.0f);
     glBegin(GL_TRIANGLES);
     glVertex3f(-1.0f, 1.0f, 0.0f);
@@ -21,7 +22,7 @@ void drawPerson() {
     glVertex3f(0.0f, -1.0f, 0.0f);
     glEnd();
 
-    // Brazos
+    // Triangulo Central
     glColor3f(0.0f, 0.0f, 1.0f);
     glBegin(GL_TRIANGLES);
     glVertex3f(-1.0f, 1.0f, 0.0f);
@@ -29,13 +30,14 @@ void drawPerson() {
     glVertex3f(-1.0f, -1.0f, 0.0f);
     glEnd();
 
+    // Triangulo Derecho
     glBegin(GL_TRIANGLES);
     glVertex3f(1.0f, 1.0f, 0.0f);
     glVertex3f(2.0f, 0.0f, 0.0f);
     glVertex3f(1.0f, -1.0f, 0.0f);
     glEnd();
 
-    // Piernas
+    // Triangulo Inferior
     glColor3f(1.0f, 1.0f, 0.0f);
     glBegin(GL_TRIANGLES);
     glVertex3f(-0.5f, -1.0f, 0.0f);
@@ -46,11 +48,12 @@ void drawPerson() {
 
 
 void display() {
+
     glClear(GL_COLOR_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    // Dibuja los ejes de coordenadas
+    // Ejes Coordenados
     glBegin(GL_LINES);
     glColor3f(0.0f, 0.0f, 1.0f);
     glVertex3f(-50.0f, 0.0f, 0.0f);
@@ -62,28 +65,28 @@ void display() {
 
     glTranslatef(x, y, 0.0f);
     glColor3f(0.0f, 1.0f, 0.0f);
-    glutSolidSphere(1.0f, 20, 20); // Dibuja una esfera en la posición actual del proyectil
+    glutSolidSphere(1.0f, 20, 20); // Esfera en la posición actual del proyectil
 
     glTranslatef(0.0f, 0.0f, 0.0f);
-    drawPerson();
+    drawFigures();
 
     glutSwapBuffers();
 }
 
+float angle = 65.0f; // Ángulo de lanzamiento en grados
+float speed = 20.0f; // Velocidad inicial en metros por segundo
+float gravity = 9.8f; // Aceleración de la gravedad en metros por segundo al cuadrado
+float time = 0.0f; // Tiempo transcurrido desde el lanzamiento en segundos
+float PI = 3.1416; //Constante PI
+
 void update(int value) {
-
-    float angle = 65.0f; // Ángulo de lanzamiento en grados
-    float speed = 20.0f; // Velocidad inicial en metros por segundo
-    float gravity = 9.8f; // Aceleración de la gravedad en metros por segundo al cuadrado
-    float time = 0.0f; // Tiempo transcurrido desde el lanzamiento en segundos
-
-    // float M_PI = 3.1416;
+    
     // Actualiza la posición del proyectil en función del tiempo transcurrido
-    x = speed * cos(angle * M_PI / 180) * time;
-    y = speed * sin(angle * M_PI / 180) * time - 0.5f * gravity * time * time;
+    x = speed * cos(angle * PI / 180) * time;
+    y = speed * sin(angle * PI / 180) * time - 0.5f * gravity * time * time;
 
-    float t_max = speed * sin(angle * M_PI / 180) / gravity;
-    float h_max = y + (speed * sin(angle * M_PI / 180)) * (speed * sin(angle * M_PI / 180)) / (2 * gravity);
+    float t_max = speed * sin(angle * PI / 180) / gravity;
+    float h_max = y + (speed * sin(angle * PI / 180)) * (speed * sin(angle * PI / 180)) / (2 * gravity);
 
     if (h_max <= 0.0f) {
         return;
@@ -92,11 +95,11 @@ void update(int value) {
     printf("Altura maxima por intervalo de tiempo: %.2f metros\n", h_max);
 
     glutPostRedisplay();
-    glutTimerFunc(16, update, 0); // Llama a esta función de nuevo después de 16 milisegundos (aproximadamente 60 FPS)
-    time += 0.016f; // Incrementa el tiempo transcurrido en 0.016 segundos (aproximadamente 60 FPS)
+    glutTimerFunc(16, update, 0); // 16 milisegundos (aproximadamente 60 FPS)
+    time += 0.016f; // Tiempo transcurrido en 0.016 segundos (aproximadamente 60 FPS)
 }
 
-int runGui(int argc, char** argv) {
+int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(640, 480);
