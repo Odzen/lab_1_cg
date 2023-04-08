9be03533a70e47146a5041cb3408bf18668188c7
Odzen <jsebastian.va@gmail.com>
2023-04-07 23:04:38 -0500

    Src code

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
-# Lab 1 - Computer Graphics
-This laboratory is about the modeling and operations of vectors in 1D, 2D and 3D dimensions. And the simulation of the movement of a projectile. 
-The tools used for the development of this laboratory were: C++, OpenGL.
+# Simulation
+Application in OpenGL that simulates projectile launch considering:
+- The initial velocity of the projectile and the time interval Δt
+- The initial position of the projectile
+- Calculation of all projectile positions every Δt seconds until the projectile has zero height.
 
-## Navigation
+## To run the simulation
 
-This repository contains the following branches:
-- [vectors](https://github.com/Odzen/lab_1_cg/tree/vectors): Contains the implementation of the Vector class and its methods.
-- [simulation](https://github.com/Odzen/lab_1_cg/tree/simulation): Contains the implementation of the simulation of the movement of a projectile.
-
-Please navigate to an specific branch to see the source code and implementation of the laboratory.
-
-## Colaborators
-- [Mavelyn Sterling Londoño](https://github.com/MavelSterling)
-- [Juan Sebastián Velasquez Acevedo](https://github.com/Odzen)
-- [Sebastián Tutistar Valencia](https://github.com/xlPlanetarizaGol)
+## Video
---
+++ main.cpp
@@ -0,0 +1,116 @@
+#include <GL/glut.h>
+#include <math.h>
+#include <stdio.h>
+
+using namespace std;
+
+float x = 0.0f; // Posición horizontal del proyectil en metros
+float y = 0.0f; // Posición vertical del proyectil en metros
+
+void drawPerson() {
+    // Cabeza
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glTranslatef(0.0f, 2.0f, 0.0f);
+    glutSolidSphere(1.0f, 20, 20);
+
+    // Torso
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glBegin(GL_TRIANGLES);
+    glVertex3f(-1.0f, 1.0f, 0.0f);
+    glVertex3f(1.0f, 1.0f, 0.0f);
+    glVertex3f(0.0f, -1.0f, 0.0f);
+    glEnd();
+
+    // Brazos
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glBegin(GL_TRIANGLES);
+    glVertex3f(-1.0f, 1.0f, 0.0f);
+    glVertex3f(-2.0f, 0.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, 0.0f);
+    glEnd();
+
+    glBegin(GL_TRIANGLES);
+    glVertex3f(1.0f, 1.0f, 0.0f);
+    glVertex3f(2.0f, 0.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, 0.0f);
+    glEnd();
+
+    // Piernas
+    glColor3f(1.0f, 1.0f, 0.0f);
+    glBegin(GL_TRIANGLES);
+    glVertex3f(-0.5f, -1.0f, 0.0f);
+    glVertex3f(0.5f, -1.0f, 0.0f);
+    glVertex3f(0.0f, -3.0f, 0.0f);
+    glEnd();
+}
+
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    // Dibuja los ejes de coordenadas
+    glBegin(GL_LINES);
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(-50.0f, 0.0f, 0.0f);
+    glVertex3f(50.0f, 0.0f, 0.0f);
+    glColor3f(1.0f, 1.0f, 0.0f);
+    glVertex3f(0.0f, -50.0f, 0.0f);
+    glVertex3f(0.0f, 50.0f, 0.0f);
+    glEnd();
+
+    glTranslatef(x, y, 0.0f);
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glutSolidSphere(1.0f, 20, 20); // Dibuja una esfera en la posición actual del proyectil
+
+    glTranslatef(0.0f, 0.0f, 0.0f);
+    drawPerson();
+
+    glutSwapBuffers();
+}
+
+void update(int value) {
+
+    float angle = 65.0f; // Ángulo de lanzamiento en grados
+    float speed = 20.0f; // Velocidad inicial en metros por segundo
+    float gravity = 9.8f; // Aceleración de la gravedad en metros por segundo al cuadrado
+    float time = 0.0f; // Tiempo transcurrido desde el lanzamiento en segundos
+
+    // float M_PI = 3.1416;
+    // Actualiza la posición del proyectil en función del tiempo transcurrido
+    x = speed * cos(angle * M_PI / 180) * time;
+    y = speed * sin(angle * M_PI / 180) * time - 0.5f * gravity * time * time;
+
+    float t_max = speed * sin(angle * M_PI / 180) / gravity;
+    float h_max = y + (speed * sin(angle * M_PI / 180)) * (speed * sin(angle * M_PI / 180)) / (2 * gravity);
+
+    if (h_max <= 0.0f) {
+        return;
+    }
+
+    printf("Altura maxima por intervalo de tiempo: %.2f metros\n", h_max);
+
+    glutPostRedisplay();
+    glutTimerFunc(16, update, 0); // Llama a esta función de nuevo después de 16 milisegundos (aproximadamente 60 FPS)
+    time += 0.016f; // Incrementa el tiempo transcurrido en 0.016 segundos (aproximadamente 60 FPS)
+}
+
+int runGui(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(640, 480);
+    glutCreateWindow("Lanzamiento de Proyectil - C++ | OpenGL - Computación Gráfica");
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-50.0f, 50.0f, -50.0f, 50.0f, -1.0f, 1.0f);
+
+    glutDisplayFunc(display);
+    glutTimerFunc(0, update, 0);
+
+    glutMainLoop();
+
+    return 0;
+}
